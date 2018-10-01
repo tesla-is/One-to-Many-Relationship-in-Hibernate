59813e55f7be1c57ac78ab35547c6bf820c6546a
Rahul <Rahul@HP>
2018-10-01 15:51:48 +0530

    Created the configuration file along with the POJO and the mapping resource in the form of an XML document.

---
+++ src/java/src/Answer.java
@@ -0,0 +1,35 @@
+package src;
+
+public class Answer {
+
+    private int id;
+    private String postedBy;
+    private String answerName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPostedBy() {
+        return postedBy;
+    }
+
+    public void setPostedBy(String postedBy) {
+        this.postedBy = postedBy;
+    }
+
+    public String getAnswerName() {
+        return answerName;
+    }
+
+    public void setAnswerName(String answerName) {
+        this.answerName = answerName;
+    }
+    
+    
+    
+}
